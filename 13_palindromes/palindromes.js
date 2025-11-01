42978be6af52f03c0d4c73aeb6317cfaa1cbda0c
ArseneBerod <arsene.berod@proton.me>
2025-11-01 10:58:19 +0100

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  return (code >= 65 && code <= 90) || (code >= 97 && code <= 122) || (code >= 48 && code <= 57);
+}
 
+const palindromes = function (string) {
+	const cleanUpString = string.toLowerCase().split('').filter((item) => {
+		return isAlphaNumeric(item);
+	}).join('');
+	const reverse = cleanUpString.split('').reverse().join('');
+	return cleanUpString === reverse;
 };
 
 // Do not edit below this line
